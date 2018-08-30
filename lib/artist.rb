be0973e0110133649110f6f9dc9e9b74cca5c39c
yapjinai <jinai.jyap@gmail.com>
2018-08-29 21:42:16 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist =  self
+  end
+
+  def add_song_by_name(name)
+    new_song = Song.new(name)
+    new_song.artist = self
+    add_song(new_song)
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+end
